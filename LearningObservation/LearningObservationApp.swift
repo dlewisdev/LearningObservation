6ab9f004cd155096b35c6be055ca66c7fe63ffce
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-15 13:18:47 -0500

    Initial Commit

---
+++ LearningObservation/LearningObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningObservationApp.swift
+//  LearningObservation
+//
+//  Created by Danielle Lewis on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
